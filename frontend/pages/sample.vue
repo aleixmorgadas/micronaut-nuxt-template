adfc15d6ea9242427742eb7446780cab9cdf93a6
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2021-09-20 09:10:45 +0200

    some api call

---
+++ frontend/pages/sample.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div>
+      <p>Name {{ sampleData.name }}</p>
+      <p>Value {{ sampleData.value }}/10</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {Context} from "@nuxt/types";
+
+interface SampleData {
+  name: String;
+  value: Number;
+}
+
+export default Vue.extend({
+  async asyncData(ctx: Context): Promise<object> {
+    const response = await fetch('http://localhost:8080/sample');
+    return {
+      sampleData: await response.json()
+    }
+  }
+})
+</script>
+
